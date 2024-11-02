eb43376fd46e7fe7db6fc7e2abb7bd6abdcf69d9
Francesco Di Sales <f.disales@hotmail.com>
2024-11-02 15:07:18 +0100

    Update testdisk-remove.sh with pwd fix

--- testdisk-remove.sh
+++ testdisk-remove.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+current_dir=$(pwd)
+directory=$1
+number=$2
 
-number=$1
+if [ "$directory" != "" ]
+then
 
-for remove_item in $(seq 1 $number)
-do
-        rm -r -f recup_dir.$remove_item
-done
+ cd $directory
 
+ for remove_item in $(seq 1 $number)
+ do
+   rm -r -f recup_dir.$remove_item
+ done
+
+ cd $current_dir
+
+else
+  echo "please pass your test-disk recover directory"
+fi
                        
